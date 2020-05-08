1383a3f86073a36581a18519f98d44a12277ee40
newheart <newheartlee@foxmail.com>
2020-05-08 10:34:17 +0800

    自己造轮子项目
    自己造轮子项目

---
+++ src/test/java/com/xinxin/zzlz/智造轮子ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinxin.zzlz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class 智造轮子ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
